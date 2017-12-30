2a06af2f1d4d4adb1f9e2dae19571b715f84ba7e
ozonar <github@okm.org.ru>
2017-12-31 02:55:11 +0700

    Rm config.php

--- config.php
+++ config.php
@@ -9,9 +9,12 @@ return [
     'charset' => "UTF8",
 
 // Гуглокаптча
-    'secret_key' => "6LfKfwYTAAAAANb9E_GrZ3zOa26KBsmQhhQCApAg",
-    'public_key' => "6LfKfwYTAAAAAIFPeT0CSy6CA9s6mfRzudDNWt78",
+//    'secret_key' => "6LfKfwYTAAAAANb9E_GrZ3zOa26KBsmQhhQCApAg",
+//    'public_key' => "6LfKfwYTAAAAAIFPeT0CSy6CA9s6mfRzudDNWt78",
+    'recaptcha_secret_key' => "6LfYZDkUAAAAAM-myzQ4SjKzYcUhi5XKaqofyqLW",
+    'recaptcha_public_key' => "6LfYZDkUAAAAAMyHrm3pcvjuNfFbZWEq2AStKT5V",
 
     'wot_secret_key' => 'bf2fa7a3025c90c9ce9d567f856ab7718e172d05',
     'min_wot_reputation' => '50',
+    'save_image_password' => '630077',
 ];
