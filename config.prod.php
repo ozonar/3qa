10205352d53f24d4d2ba681d62cfd379d9460696
ozonar <7EvilCyberBears>
2022-12-09 18:20:21 +0700

    Add prod config

---
+++ config.prod.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'dbtype' => "pgsql",
+    'dbname' => "3qa",
+    'host' => "localhost",
+    'username' => "postgres",
+    'password' => '6300',
+    'charset' => "UTF-8",
+
+
+// Гуглокаптча
+    'recaptcha_secret_key' => "6LfYZDkUAAAAAM-myzQ4SjKzYcUhi5XKaqofyqLW",
+    'recaptcha_public_key' => "6LfYZDkUAAAAAMyHrm3pcvjuNfFbZWEq2AStKT5V",
+    'save_image_password' => "630077",
+];
